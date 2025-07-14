d6a90da6210131ee436d9a1fb69effc97e2cd962
loka-harika-1045 <146203367+loka-harika-1045@users.noreply.github.com>
2025-07-14 23:09:30 +0530

    Create SumofIntegers.java

---
+++ java_fundamentals/SumofIntegers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class SumofIntegers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s1=sc.nextInt();
+        int s2=sc.nextInt();
+        System.out.print(s1+s2);
+    }
+}
