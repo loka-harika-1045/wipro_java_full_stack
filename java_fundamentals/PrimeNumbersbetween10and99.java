948da72fce02d3c221b791acf36a460797a8a254
loka-harika-1045 <146203367+loka-harika-1045@users.noreply.github.com>
2025-07-15 12:40:27 +0530

    Create PrimeNumbersbetween10and99.java

---
+++ java_fundamentals/PrimeNumbersbetween10and99.java
@@ -0,0 +1,21 @@
+public class PrimeNumbersbetween10and99{
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 10 and 99 are:");
+        for (int num = 10; num <= 99; num++) {
+            boolean isPrime = true;
+            if (num < 2) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= num / 2; i++) {
+                    if (num % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
