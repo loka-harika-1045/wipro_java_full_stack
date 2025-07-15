22877d7941538c64969594efcf7eb5568396f1d0
loka-harika-1045 <146203367+loka-harika-1045@users.noreply.github.com>
2025-07-15 15:43:25 +0530

    Create AbstractionExample1.java

---
+++ Java_Oops_concept/AbstractionExample1.java
@@ -0,0 +1,18 @@
+public class AbstractionExample1 {
+    public static void main(String[] args) {
+        Animal dog = new Dog();  
+        dog.makeSound();        
+        dog.sleep();          
+    }
+}
+abstract class Animal {
+    abstract void makeSound();
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+}
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
