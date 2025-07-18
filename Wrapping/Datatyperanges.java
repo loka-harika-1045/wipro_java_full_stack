bac9ff2ab277fb4d750018f3c3f8695007e3c232
loka-harika-1045 <146203367+loka-harika-1045@users.noreply.github.com>
2025-07-18 11:36:06 +0530

    Create Datatyperanges.java

---
+++ Wrapping/Datatyperanges.java
@@ -0,0 +1,14 @@
+package Datatyperanges;
+
+public class Datatyperanges {
+
+	public static void main(String[] args) {
+		System.out.println("Integer range : Max : " + Integer.MAX_VALUE + " Min : " + Integer.MIN_VALUE );
+		System.out.println("Double range : Max : " + Double.MAX_VALUE + " Min : " + Double.MIN_VALUE );
+		System.out.println("Float range : Max : " + Float.MAX_VALUE + " Min : " + Float.MIN_VALUE );
+		System.out.println("Long range : Max : "+ Long.MAX_VALUE + " Min : " + Long.MIN_VALUE );
+		System.out.println("Short range : Max : " + Short.MAX_VALUE + " Min : "+ Short.MIN_VALUE);
+		System.out.println("Byte range : Max : "+ Byte.MAX_VALUE + "Min : "+ Byte.MIN_VALUE);
+	}
+
+}
