b01c633779a59af37d775fd0f6c22e14983038b2
loka-harika-1045 <146203367+loka-harika-1045@users.noreply.github.com>
2025-07-18 15:02:30 +0530

    Create MonthsListArraylist.java

---
+++ ArrayList/MonthsListArraylist.java
@@ -0,0 +1,25 @@
+package Testpackage;
+
+	import java.util.ArrayList;
+	public class MonthsListArraylist {
+	    public static void main(String[] args) {
+	        ArrayList<String> months = new ArrayList<>();
+	        months.add("January");
+	        months.add("February");
+	        months.add("March");
+	        months.add("April");
+	        months.add("May");
+	        months.add("June");
+	        months.add("July");
+	        months.add("August");
+	        months.add("September");
+	        months.add("October");
+	        months.add("November");
+	        months.add("December");
+	        System.out.println("Months of the Year:");
+	        months.listIterator().previous();
+	            System.out.println(months);
+	        }
+	}
+
+  using list iterator do this
