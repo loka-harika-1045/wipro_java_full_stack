6153e946cb995dd11de017cde2893820f8abf926
Loka Harika Golla <146203367+loka-harika-1045@users.noreply.github.com>
2025-07-28 01:13:25 +0530

    Create EvenCount.java

---
+++ java_fundamentals/EvenCount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class EvenCount {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter 5 elements");
+        int count=0;
+        for(int i=0;i< 5;i++){
+           int r=sc.nextInt(); 
+           if(r % 2 == 0){
+               count++;
+           }
+        }
+        System.out.println("Even Count" + count);
+    }
+}
